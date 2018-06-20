fe705ad39d933f531cf6839f9123ab0dff493e29
Patrick Lin <patrick@patricklin.ca>
2018-06-20 01:23:54 -0700

    convert data to csv for readablility

--- Basketball Analytics/scripts/extract.py
+++ Basketball Analytics/scripts/extract.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
-def clean():
+def writeCSV():
     events = pd.read_table('../data/NBA Hackathon - Event Codes.txt')
     lineup = pd.read_table('../data/NBA Hackathon - Game Lineup Data Sample (50 Games).txt')
     plays = pd.read_table('../data/NBA Hackathon - Play by Play Data Sample (50 Games).txt')
 
+    events.to_csv('../results/event_code.csv', index=False)
+    lineup.to_csv('../results/game_lineup.csv', index=False)
+    plays.to_csv('../results/play_by_play.csv', index=False)
+
     pd.read_table('../data/NBA Hackathon - Game Lineup Data Sample (50 Games).txt', usecols=['Game_id', 'Person_id']).to_csv('../results/Q1_BBALL.csv', index=False)
+    plus_minus = pd.read_csv('../results/Q1_BBALL.csv')
+    plus_minus['Player_Plus/Minus'] = ''
+    plus_minus.to_csv('../results/Q1_BBALL.csv', index=False)   
 
-    result = pd.read_csv('../results/Q1_BBALL.csv')
-    result['Player_Plus/Minus'] = ''
-    result.to_csv('../results/Q1_BBALL.csv', index=False)
-
-    print(result)
-   
-
-clean()
+writeCSV()
