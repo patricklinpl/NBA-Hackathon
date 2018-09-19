4a1a157d2ccd472bee31c771f5121f890d020149
Patrick Lin <patrick@patricklin.ca>
2018-09-18 21:05:51 -0400

    reoder and change script for domestic

--- Scripts/google-trends-query-builder/src/index.js
+++ Scripts/google-trends-query-builder/src/index.js
@@ -4,20 +4,22 @@ import mkdirp from 'mkdirp'
 import teams from './data/nba-teams'
 
 const buildQueryString = async (date) => {
-  const base = `https://trends.google.com/trends/explore?date=${date}&q=`
-  const atlanta = `%2Fm%2F0jm64,`
-  let searchQ = `${base}${atlanta}`
+  const base = `https://trends.google.com/trends/explore?date=${date}&geo=US&q=`
+  const baseTeam = `%2Fm%2F0jm2v,`
+  let searchQ = `${base}${baseTeam}`
   const queryHolder = ['Google Trends']
   let count = 0
   const nbaTeam = Object.keys(teams)
   for (let i = 0; i < nbaTeam.length; i++) {
-    searchQ += `${teams[nbaTeam[i]]},`
-    count++
-    if (count === 4 || i === nbaTeam.length - 1) {
-      searchQ = searchQ.substring(0, searchQ.length - 1)
-      queryHolder.push(searchQ)
-      searchQ = `${base}${atlanta}`
-      count = 0
+    if (`${teams[nbaTeam[i]]},` !== baseTeam) {
+      searchQ += `${teams[nbaTeam[i]]},`
+      count++
+      if (count === 2 || i === nbaTeam.length - 1) {
+        searchQ = searchQ.substring(0, searchQ.length - 1)
+        queryHolder.push(searchQ)
+        searchQ = `${base}${baseTeam}`
+        count = 0
+      }
     }
   }
 
@@ -32,5 +34,5 @@ const buildQueryString = async (date) => {
   })
 }
 
-buildQueryString(`2016-10-01%202017-05-01`)
+// buildQueryString(`2016-10-01%202017-05-01`)
 buildQueryString(`2017-10-01%202018-05-01`)
